4aa684fa97ac9d851da7d99186925857b1d72f9c
Imthadh Ahamed <imthadh.ahamed.iahs@gmail.com>
2025-03-25 22:48:20 +0530

    feat: Modify the task management page

--- client/src/components/FocusMode/ui/card.tsx
+++ client/src/components/FocusMode/ui/card.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
-import MuiCard from '@mui/material/Card';
-import { cn } from "@/lib/utils";
+import * as React from "react";
+import MuiCard from "@mui/material/Card";
+import { cn } from "@/components/TaskManagement/lib/utils";
 
 const CustomCard = React.forwardRef<
   HTMLDivElement,
@@ -14,8 +14,8 @@ const CustomCard = React.forwardRef<
     )}
     {...props}
   />
-))
-CustomCard.displayName = "Card"
+));
+CustomCard.displayName = "Card";
 
 const CardHeader = React.forwardRef<
   HTMLDivElement,
@@ -26,8 +26,8 @@ const CardHeader = React.forwardRef<
     className={cn("flex flex-col space-y-1.5 p-6", className)}
     {...props}
   />
-))
-CardHeader.displayName = "CardHeader"
+));
+CardHeader.displayName = "CardHeader";
 
 const CardTitle = React.forwardRef<
   HTMLDivElement,
@@ -38,8 +38,8 @@ const CardTitle = React.forwardRef<
     className={cn("font-semibold leading-none tracking-tight", className)}
     {...props}
   />
-))
-CardTitle.displayName = "CardTitle"
+));
+CardTitle.displayName = "CardTitle";
 
 const CardDescription = React.forwardRef<
   HTMLDivElement,
@@ -50,16 +50,16 @@ const CardDescription = React.forwardRef<
     className={cn("text-sm text-muted-foreground", className)}
     {...props}
   />
-))
-CardDescription.displayName = "CardDescription"
+));
+CardDescription.displayName = "CardDescription";
 
 const CustomCardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
   <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
-))
-CustomCardContent.displayName = "CardContent"
+));
+CustomCardContent.displayName = "CardContent";
 
 const CardFooter = React.forwardRef<
   HTMLDivElement,
@@ -70,7 +70,14 @@ const CardFooter = React.forwardRef<
     className={cn("flex items-center p-6 pt-0", className)}
     {...props}
   />
-))
-CardFooter.displayName = "CardFooter"
+));
+CardFooter.displayName = "CardFooter";
 
-export { CustomCard as Card, CardHeader, CardFooter, CardTitle, CardDescription, CustomCardContent as CardContent }
+export {
+  CustomCard as Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CustomCardContent as CardContent,
+};
